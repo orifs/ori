a96051535251dd1c1bfe15532a2ae1c50c11fff6
Ali Mashtizadeh <mashti@cs.stanford.edu>
2012-05-31 13:42:23 -0700

    Splitting the commands from the repository code.

---
+++ ori/repo_cmd.cc
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2012 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#define _WITH_DPRINTF
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <string>
+
+#include "debug.h"
+#include "scan.h"
+#include "util.h"
+#include "repo.h"
+
+using namespace std;
+
+/********************************************************************
+ *
+ *
+ * Commands
+ *
+ *
+ ********************************************************************/
+
+
+int
+cmd_init(int argc, char *argv[])
+{
+    string rootPath;
+    string oriDir;
+    string tmpDir;
+    string versionFile;
+    string uuidFile;
+#ifdef __FreeBSD__
+    uuid_t id;
+    char *uuidBuf;
+#endif /* __FreeBSD__ */
+    uint32_t status;
+    int fd;
+    
+    if (argc == 1) {
+        char *cwd = getcwd(NULL, MAXPATHLEN);
+        rootPath = cwd;
+        free(cwd);
+    } else if (argc == 2) {
+        rootPath = argv[1];
+    } else {
+        printf("Too many arguments!\n");
+        return 1;
+    }
+
+    // Create directory
+    oriDir = rootPath + ORI_PATH_DIR;
+    if (mkdir(oriDir.c_str(), 0755) < 0) {
+        perror("Could not create '.ori' directory");
+        return 1;
+    }
+
+    // Create tmp directory
+    tmpDir = rootPath + ORI_PATH_DIR + "/tmp";
+    if (mkdir(tmpDir.c_str(), 0755) < 0) {
+        perror("Could not create '.ori/tmp' directory");
+        return 1;
+    }
+
+    // Construct UUID
+    uuidFile = rootPath + ORI_PATH_UUID;
+    fd = open(uuidFile.c_str(), O_CREAT|O_WRONLY|O_APPEND, 0660);
+    if (fd < 0) {
+        perror("Could not create UUID file");
+        return 1;
+    }
+
+#ifdef __FreeBSD__
+    uuid_create(&id, &status);
+    if (status != uuid_s_ok) {
+        printf("Failed to construct UUID!\n");
+        return 1;
+    }
+    uuid_to_string(&id, &uuidBuf, &status);
+    if (status != uuid_s_ok) {
+        printf("Failed to print UUID!\n");
+        return 1;
+    }
+    dprintf(fd, "%s", uuidBuf);
+    free(uuidBuf);
+#endif /* __FreeBSD__ */
+    close(fd);
+    chmod(uuidFile.c_str(), 0440);
+
+    // Construct version file
+    versionFile = rootPath + ORI_PATH_VERSION;
+    fd = open(versionFile.c_str(), O_CREAT|O_WRONLY|O_APPEND, 0660);
+    if (fd < 0) {
+        perror("Could not create version file");
+        return 1;
+    }
+    dprintf(fd, "ORI1.0");
+    close(fd);
+
+    return 0;
+}
+
+int
+cmd_show(int argc, char *argv[])
+{
+    string rootPath = Repo::getRootPath();
+
+    if (rootPath.compare("") == 0) {
+        printf("No repository found!\n");
+        return 1;
+    }
+
+    printf("--- Repository ---\n");
+    printf("Root: %s\n", rootPath.c_str());
+    printf("UUID: %s\n", repository.getUUID().c_str());
+    printf("Version: %s\n", repository.getVersion().c_str());
+    //printf("Peers:\n");
+    // for
+    // printf("    %s\n", hostname);
+    return 0;
+}
+
+int
+cmd_catobj(int argc, char *argv[])
+{
+    size_t len;
+    char *buf;
+    bool hex = false;
+
+    len = repository.getObjectLength(argv[1]);
+    if (len < 0) {
+	printf("Object does not exist.\n");
+	return 1;
+    }
+
+    buf = repository.getObject(argv[1]);
+
+    for (int i = 0; i < 0; i++) {
+	if (buf[i] < 1 || buf[i] >= 0x80) {
+	    hex = true;
+	    break;
+	}
+    }
+
+    if (hex) {
+	// XXX: Add hex pretty printer
+	printf("hex object!\n");
+    } else {
+	printf("%s", buf);
+    }
+
+    return 0;
+}
+
+int
+commitHelper(void *arg, const char *path)
+{
+    string hash;
+    Tree *tree = (Tree *)arg;
+
+    if (Util_IsDirectory(path)) {
+	string blob;
+	Tree subTree = Tree();
+
+	Scan_Traverse(path, &subTree, commitHelper);
+
+	blob = subTree.getBlob();
+	hash = repository.addBlob(blob);
+    } else {
+	hash = repository.addFile(path);
+    }
+    tree->addObject(path, hash);
+
+    return 0;
+}
+
+int
+cmd_commit(int argc, char *argv[])
+{
+    string blob;
+    string treeHash, commitHash;
+    Tree tree = Tree();
+    Commit commit = Commit();
+    string root = Repo::getRootPath();
+
+    Scan_Traverse(root.c_str(), &tree, commitHelper);
+
+    blob = tree.getBlob();
+    treeHash = repository.addBlob(blob);
+
+    // XXX: Get parents
+    commit.setTree(treeHash);
+    commit.setParents("1234");
+    commit.setMessage("");
+
+    commitHash = repository.addCommit(commit);
+
+    // XXX: Update .ori/tip
+
+    printf("Commit Hash: %s\nTree Hash: %s\n%s",
+	   commitHash.c_str(),
+	   treeHash.c_str(),
+	   blob.c_str());
+}
+
+int
+cmd_checkout(int argc, char *argv[])
+{
+}
+
+int
+cmd_status(int argc, char *argv[])
+{
+}
+
+int
+cmd_log(int argc, char *argv[])
+{
+}
+
