a897a4ba7820b7510910b6893407e0169d722afe
Ali Mashtizadeh <mashti@cs.stanford.edu>
2012-08-01 18:58:59 -0700

    Fixing build on Linux and probably BSD.

--- libori/fuse_cmd.h
+++ libori/fuse_cmd.h
@@ -1,4 +1,26 @@
+/*
+ * Copyright (c) 2012 Stanford University
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef __FUSE_CMD_H__
+#define __FUSE_CMD_H__
+
 #define ORI_CONTROL_FILENAME ".ori_control"
 
 std::string OF_ControlPath();
 bool OF_RunCommand(const char *cmd);
+
+#endif /* __FUSE_CMD_H__ */
+
