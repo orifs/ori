6a6009b0dd0390eb1f6679fc26544a2e54af2675
Ali Mashtizadeh <mashti@cs.stanford.edu>
2012-08-15 13:29:39 -0700

    Pushing to merge with changes on my other machine.

--- ori/cmd_merge.cc
+++ ori/cmd_merge.cc
@@ -21,7 +21,6 @@
 #include <iostream>
 #include <iomanip>
 
-#include "fuse_cmd.h"
 #include "debug.h"
 #include "util.h"
 #include "localrepo.h"
@@ -33,20 +32,43 @@ extern LocalRepo repository;
 int
 cmd_merge(int argc, const char *argv[])
 {
-    Commit c;
-    ObjectHash tip = repository.getHead();
-    if (tip != EMPTY_COMMIT) {
-        c = repository.getCommit(tip);
+    if (argc != 3) {
+	cout << "merge takes two arguments!" << endl;
+	cout << "usage: ori merge <commit 1> <commit 2>" << endl;
+	return 1;
     }
 
-    TreeDiff td;
-    td.diffToDir(c, repository.getRootPath(), &repository);
+    vector<Commit> commits = repository.listCommits();
+    vector<Commit>::iterator it;
+
+    DAG<ObjectHash, Commit> cDag = DAG<ObjectHash, Commit>();
+
+    for (it = commits.begin(); it != commits.end(); it++) {
+	cDag.addNode((*it)->hash(), (*it));
+    }
+
+    for (it = commits.begin(); it != commits.end(); it++) {
+	pair<ObjectHash, ObjectHash> p = (*it).getParents();
+	cDag.addChild(p->first, (*it)->hash());
+	if (!p->second.isEmpty())
+	    cDag.addChild(p->first, (*it)->hash());
+    }
+
+    /*TreeDiff td;
+
+    Commit c1 = repository.getCommit(ObjectHash::fromHex(argv[1]));
+    Commit c2 = repository.getCommit(ObjectHash::fromHex(argv[2]));
+
+    Tree t1 = repository.getTree(c1.getTree());
+    Tree t2 = repository.getTree(c2.getTree());
+
+    td.diffTwoTrees(t1.flattened(&repository), t2.flattened(&repository));
 
     for (size_t i = 0; i < td.entries.size(); i++) {
         printf("%c   %s\n",
                 td.entries[i].type,
                 td.entries[i].filepath.c_str());
-    }
+    }*/
 
     return 0;
 }
