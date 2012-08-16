da5e16ad8c2203611b1a8be85ef6c688588f7b19
Frank Huang <me@nongraphical.com>
2012-08-16 15:47:25 -0700

    Forgot a file

---
+++ mount_ori/ori_openedfiles.cc
@@ -0,0 +1,52 @@
+#include "ori_fuse.h"
+
+OpenedFileMgr::OpenedFileMgr()
+    : numOpenHandles(0)
+{
+}
+
+void OpenedFileMgr::openedFile(const std::string &tempFileName, int fd)
+{
+    openedFiles[tempFileName] += 1;
+    fdToFilename[fd] = tempFileName;
+    numOpenHandles += 1;
+}
+
+void OpenedFileMgr::closedFile(int fd)
+{
+    std::map<int, std::string>::iterator it = fdToFilename.find(fd);
+    assert(it != fdToFilename.end());
+
+    const std::string &tfn = (*it).second;
+    closedFile(tfn);
+}
+
+void OpenedFileMgr::closedFile(const std::string &tempFileName)
+{
+    assert(openedFiles[tempFileName] > 0);
+    assert(numOpenHandles > 0);
+
+    openedFiles[tempFileName] -= 1;
+    numOpenHandles -= 1;
+}
+
+
+void OpenedFileMgr::removeUnused()
+{
+    // TODO
+}
+
+bool OpenedFileMgr::isOpen(const std::string &tempFileName) const
+{
+    std::map<std::string, uint32_t>::const_iterator it =
+        openedFiles.find(tempFileName);
+    if (it == openedFiles.end()) {
+        return false;
+    }
+    return (*it).second > 0;
+}
+
+bool OpenedFileMgr::anyFilesOpen() const
+{
+    return numOpenHandles > 0;
+}
