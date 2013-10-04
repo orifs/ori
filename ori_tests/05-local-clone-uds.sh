72fd1e14c253fd7625078db0e633d19b6fcd5662
Ali Mashtizadeh <mashti@cs.stanford.edu>
2013-10-04 15:49:36 -0700

    Adding local-clone-uds testbench

---
+++ ori_tests/05-local-clone-uds.sh
@@ -0,0 +1,25 @@
+cd $TEMP_DIR
+
+# Mount first to use SSH over UDS socket
+$ORIFS_EXE $SOURCE_FS
+
+sleep 1
+
+$ORI_EXE replicate $SOURCE_FS $TEST_FS
+
+$ORIFS_EXE $TEST_FS
+
+sleep 1
+
+$PYTHON $SCRIPTS/compare.py "$SOURCE_FS" "$TEST_FS"
+
+$UMOUNT $SOURCE_FS
+$UMOUNT $TEST_FS
+
+cd ~/.ori/$TEST_FS.ori
+$ORIDBG_EXE verify
+$ORIDBG_EXE stats
+
+cd $TEMP_DIR
+$ORI_EXE removefs $TEST_FS
+
